84d2426efd9a1a187d35ae241b07349a5d9995f7
daneklu <84471525+daneklu@users.noreply.github.com>
2025-07-06 14:41:58 -0600

    Add files via upload

---
+++ Periodico.java
@@ -0,0 +1,31 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import java.time.LocalDate;
+
+
+
+public class Periodico extends Articulo {
+    private String issn;
+    private LocalDate fechaPublicacion;
+    private String seccionPrincipal;
+    private String paisOrigen;
+
+    public Periodico(JsonNode json) {
+        super(json);
+        this.issn = json.get("issn").asText();
+        this.fechaPublicacion = LocalDate.parse(json.get("fechaPublicacion").asText());
+        this.seccionPrincipal = json.get("seccionPrincipal").asText();
+        this.paisOrigen = json.get("paisOrigen").asText();
+    }
+
+    @Override
+    public String toString() {
+        return "Periodico{" +
+                "codigo='" + codigo + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", issn='" + issn + '\'' +
+                ", fechaPublicacion=" + fechaPublicacion +
+                ", seccionPrincipal='" + seccionPrincipal + '\'' +
+                ", paisOrigen='" + paisOrigen + '\'' +
+                '}';
+    }
+}
---
+++ Revista.java
@@ -0,0 +1,31 @@
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class Revista extends Articulo {
+    private String issn;
+    private int numeroEdicion;
+    private String mesPublicacion;
+    private TematicaRevista tematica;
+    public enum TematicaRevista {
+        TECNOLOGIA, NEGOCIOS, JARDINERIA
+    }
+    public Revista(JsonNode json) {
+        super(json);
+        this.issn = json.get("issn").asText();
+        this.numeroEdicion = json.get("numeroEdicion").asInt();
+        this.mesPublicacion = json.get("mesPublicacion").asText();
+        this.tematica = TematicaRevista.valueOf(json.get("tematica").asText());
+    }
+
+    @Override
+    public String toString() {
+        return "Revista{" +
+                "codigo='" + codigo + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", issn='" + issn + '\'' +
+                ", numeroEdicion=" + numeroEdicion +
+                ", mesPublicacion='" + mesPublicacion + '\'' +
+                ", tematica=" + tematica +
+                '}';
+    }
+}
+
